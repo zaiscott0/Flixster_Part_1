c4d8b82006ff4c148d11ff8a80086ff4ebc3033e
Zai Scott <iscott@unlockthechampion.com>
2022-12-13 22:04:04 -0500

    update

---
+++ Flixster1/app/src/main/java/com/example/flixsterkotlin/Movie.kt
@@ -0,0 +1,30 @@
+package com.example.flixsterkotlin
+
+import org.json.JSONArray
+
+data class Movie (
+    val movieId: Int,
+    private val posterPath: String,
+    val title:String,
+    val overview: String,
+    ){
+    val posterImageUrl ="https://images.tmdb.org/t/p/w342/$posterPath"
+    companion object{
+        fun fromJsonArray(movieJsonArray: JSONArray): List<Movie>{
+            val movies = mutableListOf<Movie>()
+            for( i in 0 until movieJsonArray.length()){
+                val movieJson = movieJsonArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJson.getInt("id"),
+                        movieJson.getString("poster_path"),
+                        movieJson.getString("title"),
+                        movieJson.getString("overview")
+                    )
+                )
+
+            }
+            return movies
+        }
+    }
+}
